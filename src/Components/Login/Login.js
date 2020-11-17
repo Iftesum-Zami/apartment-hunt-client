12989c796942ebe052819bdc88828a366a83ab20
mimnets <mdmonirulislammonir@gmail.com>
2020-11-17 16:59:17 +0600

    remember text added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,9 +4,10 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div>
+        <div> 
             <Navbar></Navbar>
-            <div className="d-flex justify-content-center align-items-center">
+            <div className="d-flex justify-content-center align-items-center my-5 py-5">
+                <div className="px-4 py-4" style={{border: "1px solid gray", borderRadius: "5px"}}>
                 <form className="">
                 <h2 className="text-center text-black pb-4">Login</h2>
                     <div class="form-row align-items-center">
@@ -14,6 +15,8 @@ const Login = () => {
                             {/* <label class="sr-only" for="inlineFormInput">Name</label> */}
                             <input style={{width: '350px'}} type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Username or Email "/>
                             <input style={{width: '350px'}} type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Password "/>
+                            <input type="checkBox"/> <span>Remember</span> <span>Forgot Password</span>
+                            <br/>
                             <button type="submit" class="btn btn-secondary mb-2">Login</button>
                             <h5>Dont have an account</h5>
                         </div>
@@ -22,6 +25,7 @@ const Login = () => {
                         </div>
                     </div>
                 </form>
+                </div>
             </div>
         </div>
     );
